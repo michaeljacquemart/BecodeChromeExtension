7025f8a3f278fed66484ab7f155e7b5633128920
Ashraf <ashraf@post.com>
2019-11-15 16:45:31 +0100

    Check if page isn't 404

--- BecodeCE/fetch_github.js
+++ BecodeCE/fetch_github.js
@@ -1,32 +1,29 @@
-const fetch = require("node-fetch");
 //const regex_serge = /.*(\d\d\/\d\d\/\d{4} : )(?<moi>.*)/gm;
 regex_date = /\d\d\/\d\d\/\d{4}/;
 regex_sujet = /"(.*?)"/;
 regex_nom = /\*\*(.*?)\*\*/;
 let agenda = new Array();
- 
 
-    const request = async () => {
-        const response = await fetch('https://raw.githubusercontent.com/becodeorg/The-Watch/master/LIE-Hamilton-2.12/agenda.md?token=AMECOCECLASEZRS7XJIZLFC52QSOQ');
-        const text = await response.text();
-        const lines = text.split(/\n/);
-        for(line of lines){
-            let temp = new Object();
-            temp.date = line.match(regex_date);
-            temp.nom = line.match(regex_nom);
-            temp.sujet = line.match(regex_sujet);
-            agenda.push(temp);
-        }
+const errorMessage = document.querySelector(".error-message");
+const request = async () => {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/becodeorg/The-Watch/master/LIE-Hamilton-2.12/agenda.md?token=AMECOCECLASEZRS7XJIZLFC52QSOQ"
+  );
 
-        console.log(agenda);
-        
-        
-         
-
-        
-        
-        
+  if (response.status !== 404) {
+    const text = await response.text();
+    const lines = text.split(/\n/);
+    for (line of lines) {
+      let temp = new Object();
+      temp.date = line.match(regex_date);
+      temp.nom = line.match(regex_nom);
+      temp.sujet = line.match(regex_sujet);
+      agenda.push(temp);
     }
-    
-    request();
-   
+    console.log(agenda);
+  } else {
+    errorMessage.style.display = "block";
+  }
+};
+
+request();
