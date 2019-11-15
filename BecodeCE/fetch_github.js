1a4271ce7c5c6805f52ae2085614a4430aaffdc4
Michael Jacquemart <michaeljacquemart@gmail.com>
2019-11-15 15:13:04 +0100

    changed fetch_githug

--- BecodeCE/fetch_github.js
+++ BecodeCE/fetch_github.js
@@ -1,24 +1,25 @@
 const fetch = require("node-fetch");
-//const regex = /.*(\d\d\/\d\d\/\d{4} : )(?<moi>.*)/gm;
+//const regex_serge = /.*(\d\d\/\d\d\/\d{4} : )(?<moi>.*)/gm;
 regex_date = /\d\d\/\d\d\/\d{4}/;
 regex_nom = /"(.*?)"/;
-regex_sujet = //;
-//let agenda = new Array()
-// à itérer 
-// let ligne = new Object();
-// ligne.date =
-// ligne.nom =
-// ligne.sujet =
-// agenda.push(ligne);
-
+regex_sujet = /"(.*?)"/;
+let agenda = new Array();
+ 
 
     const request = async () => {
         const response = await fetch('https://raw.githubusercontent.com/becodeorg/The-Watch/master/LIE-Hamilton-2.12/agenda.md?token=AMECOCECLASEZRS7XJIZLFC52QSOQ');
         const text = await response.text();
         const lines = text.split(/\n/);
         for(line of lines){
-            regex_date
+            let temp = new Object();
+            temp.date = line.match(regex_date);
+            temp.nom = line.match(regex_nom);
+            temp.sujet = line.match(regex_sujet);
+            agenda.push(temp);
         }
+
+        console.log(agenda);
+        
         
          
 
